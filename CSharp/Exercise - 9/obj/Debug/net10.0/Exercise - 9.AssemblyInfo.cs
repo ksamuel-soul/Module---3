357d12c591a4d16dc97024788a53f1e8bd54f93c
ksamuel-soul <ksamuel11112@gmail.com>
2026-08-17 00:05:19 +0530

    Update AssemblyInfo and cache files for multiple exercises to include new informational versioning and cache updates.

--- CSharp/Exercise - 9/obj/Debug/net10.0/Exercise - 9.AssemblyInfo.cs
+++ CSharp/Exercise - 9/obj/Debug/net10.0/Exercise - 9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise - 9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36656679adb53d690754a5a3443245fec5db9723")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise - 9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise - 9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
